8287c41f6023de3e40333fae4184e2aca31f3f38
Jonah <jonahkesslercohen@gmail.com>
2021-07-21 03:26:46 +0000

    published version!

--- commands/removemeese.py
+++ commands/removemeese.py
@@ -6,8 +6,12 @@ async def main(message, prefix, client):
 			if ":meese:" in message.content:
 				foo = message.content
 				await message.delete()
-				await client.get_channel(864644173835665458).send	(message.author.nick+": "+foo)
-			if meese.debugContainsMeese(message.content):
-				foo = message.content
-				await message.delete()
-				await client.get_channel(864644173835665458).send	(message.author.name + ": " + foo)
+				await client.get_channel(864644173835665458).send	(message.author.name+": "+foo)
+			else:
+				string = message.content
+				whitelist = config.fetch(message.guild.id, "whitelist")
+				for i in whitelist:
+					string = string.replace(i,".")
+				if meese.containsMeese(string):
+					await message.delete()
+					await client.get_channel(864644173835665458).send	(message.author.name + ": " + message.content)
