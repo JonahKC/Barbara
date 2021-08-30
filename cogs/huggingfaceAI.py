39ff74a123d44bfa0418b95ebc4db4f5138c919b
Silas Hunter-Hill <mauveshishito14@gmail.com>
2021-08-30 17:40:50 +0000

    add length parameter to %prompt, and make bandwidth use smaller

--- cogs/huggingfaceAI.py
+++ cogs/huggingfaceAI.py
@@ -8,9 +8,10 @@ GPT_NEO_URL = "https://api-inference.huggingface.co/models/EleutherAI/gpt-neo-2.
 
 headers = {"Authorization": f"Bearer {os.getenv('API_TOKEN')}"}
 
-async def query(payload, url=QA_URL):
+async def query(payload, url=QA_URL, parameters={}, options={}):
+  body = {"inputs":payload,'parameters':parameters,'options':options}
   async with aiohttp.ClientSession() as cs:
-    async with cs.post(url, headers=headers, json=payload) as response:
+    async with cs.post(url, headers=headers, json=body) as response:
       answer = await response.json()
       return answer
 
@@ -20,9 +21,11 @@ class HuggingfaceAI(commands.Cog):
     self.wikipedia = mediawiki.MediaWiki()
 
   @commands.command(name='textgen', aliases=['prompt'])
-  async def textGen(self, ctx, *, prompt: str=""):
+  async def textGen(self, ctx, length: int=-1, *, prompt: str):
     answer = await ctx.send("Waiting for GPT-NEO")
-    answerText = (await query({"inputs": prompt, "repetition_penalty": 1.3, "temperature": 2.5}, GPT_NEO_URL))[0]['generated_text']
+    if length == -1:
+      length = max(250, len(prompt))
+    answerText = prompt + (await query(prompt, GPT_NEO_URL, {"repetition_penalty": 1.3, "temperature": 2.5, "return_full_text": False, "max_length": length}))[0]['generated_text']
     await answer.edit(answerText)
 
   @commands.command(name='igotaquestion', aliases=['plzihavequestion', 'readthisandanswermyquestion', 'aiqa', 'ask'])
