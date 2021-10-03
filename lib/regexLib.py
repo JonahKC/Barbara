eb8620c141cd46111d0726180ed719ca6501c2b7
Silas Hunter-Hill <mauveshishito14@gmail.com>
2021-10-03 18:59:33 +0000

    Switch to unidecode library for meese regex, works 1,000x better now!

--- lib/regexLib.py
+++ lib/regexLib.py
@@ -1,17 +1,19 @@
 import re
+from unidecode import unidecode
 
-M_CHARS = r"mM𝔪𝖒Ⓜ𝓶𝓂Мм🇲〽⒨Պṃḿṁmസ൬നണ൩𝗆"
-E_CHARS = r"e3ėęēêèéë€𝔢𝖊€Ẹ𝒆𝓮€eе🇪ⓔ⒠ℯ∊€ḕḗḙḛḝẹẻẽếềểệễἕἔἓἒἑἐέeℰℇ∃𝖾ë"
-S_CHARS = r"sⓢ$S𝔰𝖘Ｓßšs﹩şṩṧṥṣṡഗട⒮🇸𝗌35"
-
-M_BLACKLIST = [r"/\\/\\", "♏", "♍", ":moneybag:", "dollar", ":money_with_wings:", "Ⓜ️", "️〽️", "ᛖ", "ℳ"]
-E_BLACKLIST = [":pound:", ":euro:", ":e_mail:", "∑"]
-S_BLACKLIST = [":heavy_dollar_sign:", "Տ", "տ"]
+#M_CHARS = r"mM𝔪𝖒Ⓜ🅜𝓶𝓂м🇲🄼🅼〽🄜⒨Պṃḿṁmസ൬നണ൩𝗆𝕄ꪑₘM̷ʍᶆᗰᙢጠ爪₥♍️♏️Ⓜ️〽️ℳ♏♍ᛖ𝕸ꁒᴹᵐⅯＭ𝐌𝐦𝑀𝑚𝑴𝒎𝓜𝔐𝕞𝖬𝗠𝗺𝘔𝘮𝙈𝙢𝙼𝚖"
+#E_CHARS = r"e3ėęēêèéë€𝔢𝖊𝒆𝓮eе🇪🄴🅴ⓔ🅔⒠🄔∊єḕḗḙḛḝẹẻẽếềểệễἕἔἓἒἑἐέeℰℇ∃𝖾ë∉∈≣⊑⨊⫕𝕖ꫀₑℯe̷∑६ໂ℮ěΈعεҾ£ξ𝕰ꍟ𝗘𝘌𝘦𝙀𝚎𝔼𝔈Ｅ𝐄𝐞𝑒𝑬𝓔ⅇᵉᴱȩȆȅĔ"
+#S_CHARS = r"sⓢ$S𝔰𝖘Ｓßšs﹩şṩṧṥṣṡഗട⒮🇸🅂𝗌35𝕤Ꮥѕડₛ𝓈s̷Տտȿ§Śŝ₰∫ֆక𝕾ꌚ"
+#
+M_BLACKLIST = [r"/\\/\\", ":moneybag:", "dollar", ":money_with_wings:"]
+E_BLACKLIST = [":pound:", ":euro:", ":e_mail:"]
+S_BLACKLIST = [":heavy_dollar_sign:"]
 
 TRIM_CHARS = r" \.\\/;,?!@#%^&*()"
-
-MEESE_REGEX = re.compile(r"""((([^\|][^\|])|^)["""+M_CHARS+"""]["""+E_CHARS+"""]{2,}["""+S_CHARS+"""]["""+E_CHARS+"""])""")
 TRIM_REGEX = re.compile(f"[{TRIM_CHARS}]")
+
+#MEESE_REGEX = re.compile(r"""((([^\|][^\|])|^)["""+M_CHARS+"""]["""+E_CHARS+"""]{2,}["""+S_CHARS+"""]["""+E_CHARS+"""])""")
+
 
 def replaceWords(words, string, replaceWith):
   if len(words) == 0: return string
@@ -20,10 +22,11 @@ def replaceWords(words, string, replaceWith):
   return string
 
 def containsMeese(inputStr):
-  inputStr = replaceWords(M_BLACKLIST, inputStr.lower(), "m")
-  inputStr = replaceWords(E_BLACKLIST, inputStr.lower(), "e")
-  inputStr = replaceWords(S_BLACKLIST, inputStr.lower(), "s")
-  contains_meese = re.search(MEESE_REGEX,re.sub(TRIM_REGEX, "", inputStr))
+  #inputStr = replaceWords(M_BLACKLIST, inputStr.lower(), "m")
+  #inputStr = replaceWords(E_BLACKLIST, inputStr.lower(), "e")
+  #inputStr = replaceWords(S_BLACKLIST, inputStr.lower(), "s")
+  inputStr = unidecode(inputStr)
+  contains_meese = 'meese' in re.sub(TRIM_REGEX, "", inputStr)
   if contains_meese == None:
     return False
   return True
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import config.config as config
 import lib.admin as admin
 from discord_components.client import DiscordComponents
 
-BARBARA_VERSION = '3.4.48'
+BARBARA_VERSION = '3.5.48'
 
 def get_prefix(
     bot, message
