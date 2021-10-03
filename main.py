78594973eeedc526832ad514e7c5dab944626553
Silas Hunter-Hill <mauveshishito14@gmail.com>
2021-10-03 02:23:30 +0000

    Remove duplicate %link in help command

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import config.config as config
 import lib.admin as admin
 from discord_components.client import DiscordComponents
 
-BARBARA_VERSION = '3.4.45'
+BARBARA_VERSION = '3.4.46'
 
 
 def get_prefix(
