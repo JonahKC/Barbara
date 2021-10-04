18ecb84e6be1384584e59d8b118a6fa398ad4e5d
Silas Hunter-Hill <mauveshishito14@gmail.com>
2021-10-04 04:16:27 +0000

    re-enable JCWYT's fancy-character detection on top of deunicode, for a better overall blockage of meese. Barbara 3.5.50.

--- lib/regexLib.py
+++ lib/regexLib.py
@@ -1,9 +1,9 @@
 import re
 from unidecode import unidecode
 
-#M_CHARS = r"mM𝔪𝖒Ⓜ🅜𝓶𝓂м🇲🄼🅼〽🄜⒨Պṃḿṁmസ൬നണ൩𝗆𝕄ꪑₘM̷ʍᶆᗰᙢጠ爪₥♍️♏️Ⓜ️〽️ℳ♏♍ᛖ𝕸ꁒᴹᵐⅯＭ𝐌𝐦𝑀𝑚𝑴𝒎𝓜𝔐𝕞𝖬𝗠𝗺𝘔𝘮𝙈𝙢𝙼𝚖"
-#E_CHARS = r"e3ėęēêèéë€𝔢𝖊𝒆𝓮eе🇪🄴🅴ⓔ🅔⒠🄔∊єḕḗḙḛḝẹẻẽếềểệễἕἔἓἒἑἐέeℰℇ∃𝖾ë∉∈≣⊑⨊⫕𝕖ꫀₑℯe̷∑६ໂ℮ěΈعεҾ£ξ𝕰ꍟ𝗘𝘌𝘦𝙀𝚎𝔼𝔈Ｅ𝐄𝐞𝑒𝑬𝓔ⅇᵉᴱȩȆȅĔ"
-#S_CHARS = r"sⓢ$S𝔰𝖘Ｓßšs﹩şṩṧṥṣṡഗട⒮🇸🅂𝗌35𝕤Ꮥѕડₛ𝓈s̷Տտȿ§Śŝ₰∫ֆక𝕾ꌚ"
+M_CHARS = r"mM𝔪𝖒Ⓜ🅜𝓶𝓂м🇲🄼🅼〽🄜⒨Պṃḿṁmസ൬നണ൩𝗆𝕄ꪑₘM̷ʍᶆᗰᙢጠ爪₥♍️♏️Ⓜ️〽️ℳ♏♍ᛖ𝕸ꁒᴹᵐⅯＭ𝐌𝐦𝑀𝑚𝑴𝒎𝓜𝔐𝕞𝖬𝗠𝗺𝘔𝘮𝙈𝙢𝙼𝚖"
+E_CHARS = r"e3ėęēêèéë€𝔢𝖊𝒆𝓮eе🇪🄴🅴ⓔ🅔⒠🄔∊єḕḗḙḛḝẹẻẽếềểệễἕἔἓἒἑἐέeℰℇ∃𝖾ë∉∈≣⊑⨊⫕𝕖ꫀₑℯe̷∑६ໂ℮ěΈعεҾ£ξ𝕰ꍟ𝗘𝘌𝘦𝙀𝚎𝔼𝔈Ｅ𝐄𝐞𝑒𝑬𝓔ⅇᵉᴱȩȆȅĔ"
+S_CHARS = r"sⓢ$S𝔰𝖘Ｓßšs﹩şṩṧṥṣṡഗട⒮🇸🅂𝗌35𝕤Ꮥѕડₛ𝓈s̷Տտȿ§Śŝ₰∫ֆక𝕾ꌚ"
 
 M_BLACKLIST = [r"/\\/\\", ":moneybag:", "dollar", ":money_with_wings:"]
 E_BLACKLIST = [":pound:", ":euro:", ":e_mail:"]
@@ -22,9 +22,9 @@ def replaceWords(words, string, replaceWith):
   return string
 
 def containsMeese(inputStr):
-  #inputStr = replaceWords(M_BLACKLIST, inputStr.lower(), "m")
-  #inputStr = replaceWords(E_BLACKLIST, inputStr.lower(), "e")
-  #inputStr = replaceWords(S_BLACKLIST, inputStr.lower(), "s")
+  inputStr = replaceWords(M_BLACKLIST, inputStr.lower(), "m")
+  inputStr = replaceWords(E_BLACKLIST, inputStr.lower(), "e")
+  inputStr = replaceWords(S_BLACKLIST, inputStr.lower(), "s")
   inputStr = unidecode(inputStr)
   contains_meese = re.match(MEESE_REGEX, re.sub(TRIM_REGEX, "", inputStr))
   if contains_meese == None:
