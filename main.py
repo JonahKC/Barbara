bf5906fe20ac9337e2ed8235b97f8524ed1d749f
Silas Hunter-Hill <mauveshishito14@gmail.com>
2021-10-30 06:06:47 +0000

    Singular, vulgar, pickup.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import config.config as config
 import lib.admin as admin
 from discord_components.client import DiscordComponents
 
-BARBARA_VERSION = '3.14.87'
+BARBARA_VERSION = '3.14.88'
 
 def get_prefix(
     bot, message
