128c1297084ecfa5dd53fec240e19d4e58961c69
Silas Hunter-Hill <mauveshishito14@gmail.com>
2021-11-15 06:27:07 +0000

    re-comment some rogue meese code

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import config.config as config
 import lib.admin as admin
 from discord_components.client import DiscordComponents
 
-BARBARA_VERSION = '3.16.104'
+BARBARA_VERSION = '3.16.105'
 
 def get_prefix(
     bot, message
