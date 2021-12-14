acfb6e4ab5d3d810c6b31798e01f2b00549d37c0
Silas Hunter-Hill <mauveshishito14@gmail.com>
2021-12-14 06:55:06 +0000

    update config to remove old stuff. 50% done

--- cogs/channel.py
+++ cogs/channel.py
@@ -12,23 +12,5 @@ class ChannelCommand(commands.Cog):
   async def channel(self, ctx):
     pass
 
-  @channel.command(name='rename', aliases=['ren'])
-  async def channelRename(self, ctx, channelToRename: typing.Optional[typing.Union[discord.TextChannel, discord.VoiceChannel]], newName: str):
-    if ctx.author.voice.channel != None and channelToRename == None:
-      channelToRename = ctx.author.voice.channel
-    else:
-      await ctx.send("Sorry, you must specify a channel to rename, or be in a channel.")
-      return
-    oldName = channelToRename.name
-    if admin.perms(ctx):
-        await channelToRename.edit(reason=f'{ctx.author.name} edited channel {channelToRename.name}\'s name to {newName}.', name=newName)
-    else:
-      if channelToRename.id in config.fetch(ctx.guild.id, 'publicly-renameable-channels'):
-        await channelToRename.edit(reason=f'{ctx.author.name} edited channel {channelToRename.name}\'s name to {newName}.', name=newName)
-      else:
-        await ctx.send("Sorry, you don't have the require permissions and the channel is not marked as publicly renameable.")
-        return
-    await ctx.send(f"Succesfully renamed channel `{oldName}` to `{channelToRename.name}`.")
-
 def setup(bot):
   bot.add_cog(ChannelCommand(bot))
