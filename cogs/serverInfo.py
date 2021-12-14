bad3af831025ddfde06fca0c9e46664cd1154898
Silas Hunter-Hill <mauveshishito14@gmail.com>
2021-12-14 00:54:30 +0000

    servers.json -> servers.txt, servers now update on guild join as well as bot start. Welcome now looks through every channel before silently not sending any welcome message

--- cogs/serverInfo.py
+++ cogs/serverInfo.py
@@ -1,18 +1,26 @@
-import json
 from discord.ext import commands
 
 class ServerInfo(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
 
-  @commands.Cog.listener()
-  async def on_ready(self):
+  # Function to write server names bot is currently in to a txt file
+  def write_serverlist(self):
     servers = []
     for i in self.bot.guilds:
       servers.append(i.name)
-    with open("servers.json", "w") as fp:
-      json.dump(servers,fp,indent=2,sort_keys=True)
-    return
+    with open("./servers.txt", "w") as serverlist:
+      serverlist.writelines(servers)
+  
+  # Every time the bot starts, update the serverlist
+  @commands.Cog.listener()
+  async def on_ready(self):
+    self.write_serverlist()
+
+  # We joined a new server, update servers.txt
+  @commands.Cog.listener()
+  async def on_guild_join(self, guild):
+    self.write_serverlist()
   
 def setup(bot):
   bot.add_cog(ServerInfo(bot))
