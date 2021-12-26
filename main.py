f6f5ab71dcfe5efd8c8e38e93fdeda0c9e8e3e56
Silas Hunter-Hill <mauveshishito14@gmail.com>
2021-12-26 19:50:34 +0000

    Delete channel.py because unused

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from   discord_components.client import DiscordComponents
 from   console import fg
 
 # Version constant
-BARBARA_VERSION = "3.18.119"
+BARBARA_VERSION = "3.18.120"
 
 # Pass a function to command_prefix that returns the correct per-server prefix
 def get_prefix(bot, message):
