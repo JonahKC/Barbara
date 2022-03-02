cb16af70866d1b27fe86caad658e61ce558c6cce
Silas Hunter-Hill <mauveshishito14@gmail.com>
2022-03-02 05:46:51 +0000

    Remove duplicate slash commands

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@
 SLASH_COMMANDS_GLOBAL = False
 
 # JCWYT Guild ID, for testing slash commands
-TESTING_GUILD_ID = [863919587825418241]
+TESTING_GUILD_ID = []#[863919587825418241]
 
 # JCWYT Beta Tester role ID, for whitelisting commands
 BETA_TESTER_ID = 863920057486671913
