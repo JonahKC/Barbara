355007fe8ca62bf342f8fd0095e46d196b346fac
Callum <mauveshishito14@gmail.com>
2022-07-27 01:52:40 +0000

    BLOOM 175B AI now

--- commands/prompt.py
+++ commands/prompt.py
@@ -3,100 +3,95 @@ from nextcord.ext import commands
 import lib.huggingface as huggingface
 from constants import TESTING_GUILD_ID, SLASH_COMMANDS_GLOBAL
 
+
 class PromptCommand(commands.Cog):
-  """
-	Generate text with GPT-J, the autoregressive natural language transformer by Eleuther AI.
-	"""
-  def __init__(self, bot):
-    self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-  @nextcord.slash_command(
-		name='prompt',
-		description='Generate text using the AI GPT-J.',
-		guild_ids=TESTING_GUILD_ID,
-		force_global=SLASH_COMMANDS_GLOBAL
-	)
-  async def prompt_command(
-    self,
-    interaction: nextcord.Interaction,
-    prompt: str=nextcord.SlashOption(
-      description='The AI tries to "continue" this text. Try and keep under 3 sentences.'
-    ),
-    temperature: float=nextcord.SlashOption(
-      description='Creativity/Chaos of the output.',
-      choices = {
-        'Factual': 0.0000001,
-        'Insightive': 0.5,
-        'Creative': 0.7,
-        'Very Creative': 1.0,
-        'Insane': 100.0
-      }
-    ),
-    length: int=nextcord.SlashOption(
-      description='Rough estimate of output length.',
-      choices = {
-        'One-Word': 1,
-        'Sentence': 16,
-        'Paragraph': 38,
-        'Essay': 250
-      }
-    ),
-    repetition_penalty: float=nextcord.SlashOption(
-      description='Encourage the AI to talk about new things -- or at least to not repeat words it\'s used a lot',
-      required=False,
-      default=2.0,
-      choices = {
-        "Repetetive": 0.0,
-        "Normal": 2.0,
-        "Unique": 15.0,
-        "Quirky": 50.0,
-        "ADHD": 100.0
-      }
-    ),
-    return_sequences: int=nextcord.SlashOption(
-      description='How many times should you enslave the AI to complete your prompt?',
-      required=False,
-      default=1,
-      min_value=1,
-      max_value=3
-    )
-  ):
+    @nextcord.slash_command(
+        name='prompt',
+        description=
+        'Generate text using BLOOM, the model made by the volunteers during the BigScience 1-year AI workshop',
+        guild_ids=TESTING_GUILD_ID,
+        force_global=SLASH_COMMANDS_GLOBAL)
+    async def prompt_command(
+        self,
+        interaction: nextcord.Interaction,
+        prompt: str = nextcord.SlashOption(
+            description=
+            'The AI tries to "continue" this text. Try and keep under 3 sentences.'
+        ),
+        temperature: float = nextcord.SlashOption(
+            description='Creativity/Chaos of the output.',
+            choices={
+                'Factual': 0.0000001,
+                'Insightive': 0.5,
+                'Creative': 0.7,
+                'Very Creative': 1.0,
+                'Insane': 100.0
+            }),
+        length: int = nextcord.SlashOption(
+            description='Rough estimate of output length.',
+            choices={
+                'One-Word': 1,
+                'Sentence': 16,
+                'Paragraph': 38,
+                'Essay': 250
+            }),
+        repetition_penalty: float = nextcord.SlashOption(
+            description=
+            'Encourage the AI to talk about new things -- or at least to not repeat words it\'s used a lot',
+            required=False,
+            default=2.0,
+            choices={
+                "Repetetive": 0.0,
+                "Normal": 2.0,
+                "Unique": 15.0,
+                "Quirky": 50.0,
+                "ADHD": 100.0
+            }),
+        return_sequences: int = nextcord.SlashOption(
+            description=
+            'How many times should you enslave the AI to complete your prompt?',
+            required=False,
+            default=1,
+            min_value=1,
+            max_value=3)):
 
-    # If there, remove extra leading/trailing whitespace
-    prompt = prompt.strip('\n ')
-    
-    # Bot is thinking
-    await interaction.response.defer()
+        # If there, remove extra leading/trailing whitespace
+        prompt = prompt.strip('\n ')
 
-    # Get the output from GPT-J
-    raw_ai_output = await huggingface.query(
-      prompt,
-      huggingface.Model.GPT_J_6B,
-      {
-        'repetition_penalty': repetition_penalty,
-        'num_return_sequences': return_sequences,
-        'temperature': temperature,
-        'max_new_tokens': length,
-        'return_full_text': False
-      },
-      {'wait_for_model': True}
-    )
+        # Bot is thinking
+        await interaction.response.defer()
 
-    # Loop through all of the outputs
-    for output in raw_ai_output:
+        # Get the output from GPT-J
+        raw_ai_output = await huggingface.query(
+            prompt, huggingface.Model.BLOOM, {
+                'repetition_penalty': repetition_penalty,
+                'num_return_sequences': return_sequences,
+                'temperature': temperature,
+                'max_new_tokens': length,
+                'return_full_text': False
+            }, {'wait_for_model': True})
 
-      # Add the prompt to the beginning
-      if type(output) == str:
-        complete_text = "**"+prompt+"**" + output
-      else:
-        complete_text = "**"+prompt+"**" + output['generated_text']
+        # Loop through all of the outputs
+        for output in raw_ai_output:
 
-      # Cut it off at 2000 characters so Discord doesn't scream at us
-      complete_text = complete_text[:1996] + (complete_text[1996:] and '...')
+            # Add the prompt to the beginning
+            if type(output) == str:
+                complete_text = "**" + prompt + "**" + output
+            else:
+                complete_text = "**" + prompt + "**" + output[
+                    'generated_text'][len(prompt) - 1:]
 
-      # Send a message with the prompt test as well as 
-      # the AI-generated text
-      await interaction.send(complete_text)
+            # Cut it off at 2000 characters so Discord doesn't scream at us
+            complete_text = complete_text[:1996] + (complete_text[1996:]
+                                                    and '...')
+
+            # Send a message with the prompt test as well as
+            # the AI-generated text
+            await interaction.send(complete_text)
+
 
 def setup(bot):
-  bot.add_cog(PromptCommand(bot))
+    bot.add_cog(PromptCommand(bot))
--- lib/huggingface.py
+++ lib/huggingface.py
@@ -18,15 +18,18 @@ session = None
 # The base URL for accessing Huggingface models
 _BASE_URL = "https://api-inference.huggingface.co/models/"
 
+
 class Model(enum.Enum):
-  """
+    """
   Enum representing various models and their IDs for Huggingface. This only really becomes useful when there's a lot more options to choose from. For now it looks hella cool though.
   """
-  GPT_J_6B = "EleutherAI/gpt-j-6B"
-  GPT_NEO_2B = "EleutherAI/gpt-neo-2.7B"
+    GPT_J_6B = "EleutherAI/gpt-j-6B"
+    GPT_NEO_2B = "EleutherAI/gpt-neo-2.7B"
+    BLOOM = "bigscience/bloom"
+
 
 async def query(payload, model: Model, parameters={}, options={}):
-  """
+    """
   Pass input text into an AI in the Huggingface Accelerated Inference API
   
   `payload`: Input text for AI
@@ -34,9 +37,11 @@ async def query(payload, model: Model, parameters={}, options={}):
   `parameters`: JSON object with data about how the AI should process the text. Example: `{"repetition_penalty": float (>=0.0, <=100.0), "temperature": float (>0.0, <=100.0), "return_full_text": bool, "top_p": float (>0.0, <=1.0)}`
   `options`: Non-AI-related options for how Huggingface processes the request. Example: `{'wait_for_model': bool}`
   """
-  body = {"inputs": payload, 'parameters': parameters, 'options': options}
-  async with session.post(_BASE_URL + model.value, headers=_headers, json=body) as response:
-    try:
-      return (await response.json())
-    except aiohttp.client_exceptions.ContentTypeError:
-      return (await response.text())
+    body = {"inputs": payload, 'parameters': parameters, 'options': options}
+    async with session.post(_BASE_URL + model.value,
+                            headers=_headers,
+                            json=body) as response:
+        try:
+            return (await response.json())
+        except aiohttp.client_exceptions.ContentTypeError:
+            return (await response.text())
